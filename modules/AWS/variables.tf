4b7891373640134a8b028d273a3926a4279ba757
SeanGuoAU <jeczz137@gmail.com>
2023-08-23 23:52:20 +1000

    uncomment aws account

--- modules/AWS/variables.tf
+++ modules/AWS/variables.tf
@@ -15,15 +15,3 @@ variable "layer_3_1_name" {
 
 variable "layer_3_2_name" {
 }
-
-# variable "pre-prod_account_name" {
-# }
-
-# variable "prod_account_name" {
-# }
-
-# variable "pre-prod_account_email" {
-# }
-
-# variable "prod_account_email" {
-# }
